260572d4221da87baca6c09be9c2392b8f9f276b
mili Arsema <meleymm88@gmail.com>
2026-04-16 13:27:20 -0500

    built scanner for customer input

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -1,11 +1,12 @@
 package com.pluralsight;
 
-
+import java.util.Scanner;
 
 public class SandwichShop {
     public static void main(String[] args) {
 
-
+        //build an input system
+        Scanner choice = new Scanner(System.in);
 
     }
 
