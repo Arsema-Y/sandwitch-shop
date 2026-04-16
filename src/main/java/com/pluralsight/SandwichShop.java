2b9cd342137e4b7ad6a3168a498acbe837e170b0
mili Arsema <meleymm88@gmail.com>
2026-04-16 13:45:00 -0500

    Setup interactive menu

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -16,6 +16,26 @@ public class SandwichShop {
         double discount1Price2 = basePrice2 - (basePrice2 * 0.1);
         double discount2Price2 = basePrice2 - (basePrice2 * 0.2);
 
+        //Start output + input process
+
+        System.out.println("Welcome to sandwich shop!\n");
+
+
+
+        //Set choices / menu
+        System.out.println("We have the following sizes:");
+        System.out.println("1: Regular ....  $" + basePrice1);
+        System.out.println("2: Large   ....  $" + basePrice2);
+
+        System.out.println("\nWhich one would you like?");
+        int size = choice.nextInt();
+
+        // Clear Buffer
+        choice.nextLine();
+
+        //set option for discount
+        System.out.println("We have some dicounts, would you like to check eligibility?\n Y (Yes)\n N (No) ");
+        String check = choice.nextLine();
 
     }
 
