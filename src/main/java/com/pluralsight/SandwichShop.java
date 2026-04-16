5f4b20f850cf036552099782278c3be41bdd32bb
mili Arsema <meleymm88@gmail.com>
2026-04-16 13:41:58 -0500

    Set different prices with/without discounts

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -8,6 +8,15 @@ public class SandwichShop {
         //build an input system
         Scanner choice = new Scanner(System.in);
 
+        //pre-set prices
+        double basePrice1 = 5.45;
+        double discount1Price1 = basePrice1 - (basePrice1 * 0.1);
+        double discount2Price1 = basePrice1 - (basePrice1 * 0.2);
+        double basePrice2 = 8.95;
+        double discount1Price2 = basePrice2 - (basePrice2 * 0.1);
+        double discount2Price2 = basePrice2 - (basePrice2 * 0.2);
+
+
     }
 
 }
